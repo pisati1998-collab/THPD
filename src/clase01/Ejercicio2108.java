c0bab67232589c3986d0bbb5a941e714ffab057f
florenciapatinio <57376422+florenciapatinio@users.noreply.github.com>
2025-08-21 22:45:24 -0300

    nuevo ejercicio

---
+++ src/clase01/Ejercicio2108.java
@@ -0,0 +1,46 @@
+package clase01;
+
+import java.util.Scanner;
+
+public class Ejercicio2108 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double nota1 , nota2, nota3, porcentaje;
+		System.out.println("Ingrese la nota 1");
+		nota1 = Double.parseDouble(input.nextLine());
+		System.out.println("Ingrese la nota 2");
+		nota2 = Double.parseDouble(input.nextLine());
+		System.out.println("Ingrese la nota 3");
+		nota3 = Double.parseDouble(input.nextLine());
+		boolean sacoUnDiez , primeraDivision, segundaDivision, terceraDivision;
+		porcentaje = 10*(nota1 + nota2+ nota3)/3;
+		sacoUnDiez = nota1 == 10 || nota2 == 10 || nota3 == 10;
+		primeraDivision = porcentaje >= 60 && sacoUnDiez;
+		segundaDivision = (porcentaje >=50 );
+		terceraDivision = porcentaje >= 40 && porcentaje <=49;
+		
+		
+		if(primeraDivision) {
+			System.out.println("Clasifico a la Primera Division");
+		}else if(segundaDivision) {
+			System.out.println("Clasifico a la Segunda Division");
+		}else if(terceraDivision) {
+			System.out.println("Clasifico a la tercera division");
+			
+		}else {
+			System.out.println("Desaprovado");
+		}
+		
+		
+		System.out.println(porcentaje); 
+		//System.out.println(sacoUnDiez); comprobado
+		
+		input.close();
+ 
+				
+		
+
+	}
+
+}
---
+++ src/tp1/ejercicio19.java
@@ -0,0 +1,10 @@
+package tp1;
+
+public class ejercicio19 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
